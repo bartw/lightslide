24c05f10e087f24b3871515ad79ab5879a92a35c
Bart Wijnants <bartwijnants@outlook.com>
2017-02-11 09:39:26 +0100

    empty reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const initialState = {
+    slides: [],
+    url: '',
+    currentIndex: 0,
+    showSlideShow: false
+};
+
+const lightSlideApp = function (state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default lightSlideApp;
